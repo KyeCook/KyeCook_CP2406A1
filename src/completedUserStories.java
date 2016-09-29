dba59ea80623799036da864892cfbf9d2ecde87d
KyeCook <acfanatics@hotmail.com>
2016-09-29 12:27:26 +1000

    Initial commit

---
+++ src/completedUserStories.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Kye Cook on 29/09/2016.
+ *
+ * Github URL : 
+ */
+public class completedUserStories {
+    public static void main(String[]args)
+    {
+        showWelcome();
+        showMenu();
+    }
+
+    private static void showMenu() {
+        System.out.println("\t1. Start Game");
+        System.out.println("\t2. Exit");
+    }
+
+    private static void showWelcome() {
+        System.out.println("Welcome to Mineral Supertrumps!");
+    }
+}
