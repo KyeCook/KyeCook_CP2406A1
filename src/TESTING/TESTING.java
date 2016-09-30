6ba3df666b4099625485441a24d6efe8c7caade8
KyeCook <acfanatics@hotmail.com>
2016-09-30 11:45:50 +1000

    Tested console inputs and outputs via Scanner utility

--- src/TESTING/TESTING.java
+++ src/TESTING/TESTING.java
@@ -1,14 +1,38 @@
 package TESTING;
 
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by ACfan on 29/09/2016.
  */
 public class TESTING {
     public static void main(String[]args){
-        int i = new Random().nextInt(2 + 1);
-        System.out.println(i);
+        String str;
+
+//        int i = new Random().nextInt(2 + 1);
+//        System.out.println(i);
+
+        char play = 'p';
+        char quit = 'q';
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Menu Choice\n\t>>>");
+        str = in.nextLine();
+
+        if(str.charAt(0) == 'p'){
+            System.out.println("Game has been started");
+        }
+
+        else if(str.charAt(0) == 'q'){
+            System.out.println("Game has been quit");
+        }
+
+        else {
+            System.out.printf("Incorrect input");
+        }
+
+
+//        System.out.println(str);
     }
 
 
